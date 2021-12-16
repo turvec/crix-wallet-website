07ce8536873b35320e6cb0cabc6b5ff93c540c3f
turvec <okaforturvec@gmail.com>
2021-12-16 16:22:52 +0100

    changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\CrixMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $title = 'Hurry!!!';
+        $message = 'We are pushing forward';
+
+        Mail::to(Auth::user())->send(new CrixMail($title, $message));
         $transactions = Auth::user()->transactions()->take(6)->get();
         
         return view('user.dashboard', compact('transactions'));
---
+++ app/Mail/CrixMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use PhpParser\Node\Expr\Cast\String_;
+
+class CrixMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+    public $title,$message;
+
+    public function __construct(String $title, String $message)
+    {
+        $this->title = $title;
+        $this->message = $message;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.crixmail')->subject($this->title);
+    }
+}
