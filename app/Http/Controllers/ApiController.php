cd6f8ed6aa6956b5fc5e1a23ed736173431d33d9
turvec <okaforturvec@gmail.com>
2021-12-17 15:01:06 +0100

    changes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction;
+use App\User;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+    public function users(Type $var = null)
+    {
+        $users = User::all();
+        $transactions = Transaction::all();
+        $pending = Transaction::where('status', 'pending');
+        $amount = Transaction::where('amount', '>' , 500)->get();
+    }
     //
 }
